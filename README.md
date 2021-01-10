497a21ba02f474fffeb7de09bf9480513abed9d0
lukelee <lukelee01@gmail.com>
2021-01-10 18:05:13 -0500

    update README.md. update init.sql

--- README.md
+++ README.md
@@ -3,3 +3,35 @@
 ### How to run docker containers.
     $ docker-compose build
     $ docker-compose up
+
+### credential
+
+By default, the password verification is disable(password never expired)
+Connect database with following setting:
+
+    hostname: localhost
+
+    port: 49161
+
+    sid: xe
+
+    username: system
+
+    password: oracle
+
+
+init.sql generate dummy user after db docker container initialized.
+
+    hostname: localhost
+
+    port: 49161
+
+    sid: xe
+
+    username: testuser
+
+    password: password
+    
+
+### Reference
+https://hub.docker.com/r/oracleinanutshell/oracle-xe-11g
--- oracle-xe-11g/init.sql
+++ oracle-xe-11g/init.sql
@@ -1,2 +1,19 @@
+-- create testuser
 create user testuser identified by password;
-grant create session to testuser;
+GRANT ALL PRIVILEGES TO testuser;
+
+-- connect with testuser
+CONNECT testuser/password;
+ALTER SESSION SET CURRENT_SCHEMA = testuser;
+
+-- create dummy table
+CREATE TABLE family (
+  member_id INT NOT NULL,
+  name VARCHAR(50),
+  relation VARCHAR(50)
+);
+
+-- insert dummy data into the dummy table
+INSERT INTO family (member_id, name, relation) VALUES (1, 'luke1', 'myself');
+INSERT INTO family (member_id, name, relation) VALUES (2, 'luke2', 'myself');
+INSERT INTO family (member_id, name, relation) VALUES (3, 'luke3', 'myself');
